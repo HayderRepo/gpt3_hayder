b220b30625144669651cb153d8ab4ba592c808a2
haider <rabeeahhaydeer@gmail.com>
2022-11-26 11:31:08 +0300

    finish the features component .

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import {
   Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import { Playground } from "./components";
 import './App.css'
 const App = () => {
   return <div className="App">
     <div className="gradient__bg">
-        <Navbar/>
+      <Navbar/>
         <Header/>
+       
     </div>
     <Brand/>
     <WhatGPT3/>
